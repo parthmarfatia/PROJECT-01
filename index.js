e7d4bb52ee0502f8102ef41c824e74eb596b91f3
Parth Marfatia <parthmarfatia153@gmail.com>
2024-09-15 01:39:40 +0530

    http status

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const PORT = 8000
 
 app.use(express.urlencoded({extended: false}))
 
+app.use((req, res, next) => {
+    fs.appendFile('log.txt', `${Date.now()}: ${req.method}: ${req.path}\n`,(err, data) => {
+        next()
+    })
+})
+
 
 app.get('/users',(req, res) => {
     const html = `
@@ -23,9 +29,12 @@ app.route('/api/users')
 })
 .post((req, res) => {
     const body = req.body
+    if(!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title) {
+        return res.status(400).json({message:'All fields are required'})
+    }
     users.push({...body, id: users.length + 1})
     fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data)=>{
-        return res.json({status: "success", id: users.length})
+        return res.status(201).json({status: "success", id: users.length})
     })
 })
 
@@ -34,7 +43,9 @@ app.route('/api/users')
 app.route('/api/users/:id')
 .get((req, res) => {
     const id = Number(req.params.id)
-    return res.json(users.find((user)=> user.id === id))
+    const user = users.find((user)=> user.id === id)
+    if(!user) return res.status(404).json({error:"user not found"})
+    return res.json(user)
 })
 .patch((req, res) => {
     const id = Number(req.params.id)
