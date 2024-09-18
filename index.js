c8e118b7f5d5939c50eccfd6a8280c46dbd2411b
Parth Marfatia <parthmarfatia153@gmail.com>
2024-09-19 00:36:59 +0530

    mvc architecture

--- index.js
+++ index.js
@@ -1,100 +1,16 @@
 const express = require('express')
-const fs = require('fs')
-const mongoose = require('mongoose');
+const { connectMongoDb } = require('./connection') 
+const userRouter = require('./routes/user')
+const {logReqRes} = require('./middlewares')
 
 const app = express();
 const PORT = 8000
 
-mongoose.connect("mongodb://127.0.0.1:27017/youtube-app-1")
-.then(()=>console.log('mongo connected'))
-.catch(err=>console.log(err))
-
-const userSchema = new mongoose.Schema({
-    firstName:{
-        type: String,
-        required: true
-    },
-    lastName:{
-        type: String
-    },
-    email:{
-        type: String,
-        required: true,
-        unique: true
-    },
-    jobTitle:{
-        type: String
-    },
-    gender:{
-        type: String
-    }
-},{timestamps: true})
-
-const User = mongoose.model("user", userSchema)
+connectMongoDb("mongodb://127.0.0.1:27017/youtube-app-1").then(()=>console.log('mongo connected'))
 
 app.use(express.urlencoded({extended: false}))
+app.use(logReqRes('log.txt'))
 
-app.use((req, res, next) => {
-    fs.appendFile('log.txt', `${Date.now()}: ${req.method}: ${req.path}\n`,(err, data) => {
-        next()
-    })
-})
-
-
-app.get('/users',async (req, res) => {
-    const allDbUsers = await User.find({})
-    const html = `
-        <ul>
-            ${allDbUsers.map((user)=> `<li>${user.firstName}-${user.email}</li>`).join('')}
-        </ul>
-    `
-    res.send(html)
-})
-
-app.route('/api/users')
-.get(async (req, res) => {
-    const allDbUsers = await User.find({})
-    return res.json(allDbUsers)
-})
-.post(async (req, res) => {
-    const body = req.body
-    if(!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title) {
-        return res.status(400).json({message:'All fields are required'})
-    }
-    const result = await User.create({
-        firstName: body.first_name,
-        lastName: body.last_name,
-        email: body.email,
-        gender: body.gender,
-        jobTitle: body.job_title
-    })
-    console.log('result', result)
-    return res.status(201).json({message:'Success'})
-})
-
-
-
-app.route('/api/users/:id')
-.get(async (req, res) => {
-    const id = Number(req.params.id)
-    const user = await User.findById(req.params.id)
-    if(!user) return res.status(404).json({error:"user not found"})
-    return res.json(user)
-})
-.patch(async (req, res) => {
-    const body = req.body
-    await User.findByIdAndUpdate(req.params.id, {
-        firstName: body.first_name,
-        lastName: body.last_name,
-        email: body.email,
-        gender: body.gender,
-        jobTitle: body.job_title
-    })
-    return res.json({status: "success"})
-})
-.delete(async (req, res) => {
-   await User.findByIdAndDelete(req.params.id)
-   return res.json({status: "Success"})
-})
+app.use("/api/users", userRouter)
 
 app.listen(PORT,()=>console.log('Server has started'))
